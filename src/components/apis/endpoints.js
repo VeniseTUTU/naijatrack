4a9c60b355962aa93d17dc049b64360e0e924417
VeniseTUTU <myerzpedd@gmail.com>
2020-09-13 17:23:19 -0700

    added enpoints

---
+++ src/components/apis/endpoints.js
@@ -0,0 +1,19 @@
+
+export const AllMovies = () => {
+  return "movies/0";
+};
+export const UserGenres = () => {
+  return "usergenre/31045497";
+};
+export const UserHistory = () => {
+  return "userhistory/0";
+};
+export const OneVideo = () => {
+  return 'video/'/*{userid}/{itemid}*/;
+};
+export const CreatePlaylists = () => {
+  return 'createqueue/u';
+};
+
+
+
