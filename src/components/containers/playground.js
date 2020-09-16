aa4540dd1da39b7d0b46ac2132d698e64030860d
VeniseTUTU <myerzpedd@gmail.com>
2020-09-15 20:20:30 -0700

    Refactored component

--- src/components/containers/playground.js
+++ src/components/containers/playground.js
@@ -1,65 +1,71 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
+import { NavLink, Link } from 'react-router-dom';
 import {Helmet} from "react-helmet";
-import _ from 'underscore';
 import Indicators from '../uiSources/indicators';
 import Chartoptions from '../uiSources/Chartoptions';
 import '../../scss/playground.scss';
+import {PlaygroundProvider} from '../../components/providers';
 const BASE_URL = process.env.NODE_ENV === 'development' 
 		? process.env.USE_BASE_URL_ENV
 		: process.env.USE_BASE_URL;
 
-const Playground = () => {
+const Playground = (props) => {
  
-    
-        return(
+ 
+    return(
 
-<React.Fragment>
+<Fragment>
 <Helmet>
-<title>VISUALIZE SDGs DATA IN AFRICA: Visualize SDGs data in Charts</title>
+<title>VISUALIZE SDGs DATA IN NIGERIA: Visualize SDGs data in Charts</title>
 <meta name="description" content="Visualize SDGs data in charts on the 17 goals, 169 targets, and 232 indicators in all 54 African countries." />
 <meta name="keywords" content="SDG, SDGs, Visualize, SDGs Data, 54 Countries in Africa, 169 targets, indicators,line chart, bar chart, polar chart" />
 </Helmet>  
 
-{
-isLoaded == false && ( <div className="spinnerLoad"></div>)
-}
-    
-	 <div className="mobPadding" style={{marginTop:'40px'}}> </div> 
+<PlaygroundProvider match={props.match}>
+	{({Input,data,countries,states,indicator,bearers,queryCountry,toggleHideYear}) => (
 
-	<div className="indexPad" onClick={this._toggleHideYear}>
-	
-	<Indicators indicatortitle={`${indicator.indicator}`}/>
-	
-          <div>
-	    <div className="locationBoxCont">
-	      <img src={`${BASE_URL}shades/images/jloc.png`} />
-	       <select className="locationBox" id="" onChange={this._queryCountry} defaultValue={country}>
-		<option value="Nigeria">Nigeria</option>
-	{
-	countries.map((country,index) => (
-	
-	 <option key={index} value={country.value}>{country.value}</option>	   
-	
-	))	
-		
-	}
-		</select>
-	    </div>
-	 </div>
- <div style={{position:'relative',width:'100%'}}>
+<Fragment>
+<div className="indexPad" onClick={toggleHideYear}>
 
-{  noData ?
-     <div className="noData">
-        NO DATA
-     </div> :''
+<div className="TopStrip">
+    <div className="TopStrip__Back">
+	<Link to="/visualize">   &laquo; Back </Link>
+	</div>
+
+	<div className="TopStrip__locationBoxCont">
+	  <img src={`${BASE_URL}images/jloc.png`} />
+	<select className="TopStrip__locationBoxCont__locationBox" id="" onChange={queryCountry} defaultValue={Input.country}>
+	<option value="Nigeria">Nigeria</option>
+   </select>
+	</div>
+
+	<button className="TopStrip__ReportButton">
+       View Report
+	</button>
+</div>
+
+<Indicators indicatortitle={`${indicator.indicator}`}/>
+
+
+<div style={{position:'relative',width:'100%'}}>
+
+{  Input.noData ?
+ <div className="noData">NO DATA</div> :''
 }   
-        <Chartoptions filtered={data} states={sortedStates} indicator={shortIndicator} country={country}/> 
-	<div className="source">Source(s): {`${bearers}`} </div>
- 
+	<Chartoptions filtered={data} states={states} indicator={Input.shortIndicator} country={Input.country}/> 
+    <div className="source">Source(s): {`${bearers}`} </div>
+
 
 </div>
 </div>
-</React.Fragment>
+</Fragment>
+
+)}
+
+</PlaygroundProvider>
+</Fragment>
+    
+	 
     );
     
 }
