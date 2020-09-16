f89fc5376e6d81e551dc654cb5ba0d6a03ba51de
VeniseTUTU <myerzpedd@gmail.com>
2020-09-15 20:30:33 -0700

    Imported report provider

--- src/components/providers/index.js
+++ src/components/providers/index.js
@@ -1,6 +1,7 @@
 import PlaygroundProvider from './Playground/PlaygroundProvider';
-
+import ReportProvider from './Report/ReportProvider';
 
 export {
     PlaygroundProvider,
+    ReportProvider,
 };
